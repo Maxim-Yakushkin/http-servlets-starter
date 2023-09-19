5e0706ae53e62c971ff3ac3a4a17559e8cad5b8e
Maxim_Yakushkin <max-yakushkin1@yandex.ru>
2023-09-19 23:47:45 +0300

    Initial commit

---
+++ src/main/java/com/yakushkin/http/socket/SocketRunner.java
@@ -0,0 +1,27 @@
+package com.yakushkin.http.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class SocketRunner {
+    public static void main(String[] args) throws IOException {
+        //        TCP порты
+        // http - 80
+        // https - 443
+        final InetAddress inetAddress = Inet4Address.getByName("google.com");
+        try (Socket socket = new Socket(inetAddress, 80);
+             final var outputStream = new DataOutputStream(socket.getOutputStream());
+             final var inputStream = new DataInputStream(socket.getInputStream())) {
+
+            outputStream.writeUTF("Hello World!");
+            final byte[] response = inputStream.readAllBytes();
+            System.out.println(response.length);
+            System.out.println(new String(response, StandardCharsets.UTF_8));
+        }
+    }
+}
